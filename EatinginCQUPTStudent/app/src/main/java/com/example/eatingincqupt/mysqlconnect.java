a149fc7d5b200596b45f789b4f7357cff218d58c
JinYingxu <83045867+JinYingxu@users.noreply.github.com>
2021-05-25 08:28:36 +0800

    Update mysqlconnect.java

--- EatinginCQUPTStudent/app/src/main/java/com/example/eatingincqupt/mysqlconnect.java
+++ EatinginCQUPTStudent/app/src/main/java/com/example/eatingincqupt/mysqlconnect.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class mysqlconnect {
     final static String diver = "com.mysql.jdbc.Driver";
     //加入utf-8是为了后面往表中输入中文，表中不会出现乱码的情况
-    final static String url = "jdbc:mysql://修改为自己的阿里云数据库网址:3306/eatingincqupt?characterEncoding=utf-8";
+    final static String url = "jdbc:mysql:修改为自己的阿里云数据库网址:3306/eatingincqupt?characterEncoding=utf-8";
     final static String user = "用户名";
     final static String password = "密码";
 
